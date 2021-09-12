594333cc029f7a50f5e31c67e145f67ad2e7cc81
huanmingchang <huanming1988@gmail.com>
2021-09-12 11:14:22 +0800

    feat: add interaction javascript

--- src/main.js
+++ src/main.js
@@ -106,3 +106,55 @@ let step = 0
      `
   })
 })()
+
+function handleCardClicked({ target }) {
+  const cards = cardList.querySelectorAll('.card')
+  const node = target.closest('.card')
+  if (node) {
+    const idArr = node.id.split('-')
+    const num = Number(idArr[idArr.length - 1])
+    cards[activeCard].classList.remove('active')
+    cards[num - 1].classList.add('active')
+    activeCard = num - 1
+  }
+}
+
+function handleBtnControlClicked(e) {
+  e.preventDefault()
+  const nowStep = steps[step]
+  if (e.target.matches('.btn-primary') && e.target.innerHTML === '下一步') {
+    const nextStep = steps[step + 1]
+    nowStep.classList.remove('active')
+    nowStep.classList.add('checked')
+    nextStep.classList.add('active')
+    formParts[step].classList.toggle('d-none')
+    formParts[step + 1].classList.toggle('d-none')
+    step += 1
+  } else if (e.target.matches('.btn-outline')) {
+    const prevStep = steps[step - 1]
+    nowStep.classList.remove('active')
+    prevStep.classList.remove('checked')
+    prevStep.classList.add('active')
+    formParts[step].classList.toggle('d-none')
+    formParts[step - 1].classList.toggle('d-none')
+    step -= 1
+  }
+  setBtnDisabled()
+}
+
+function setBtnDisabled() {
+  if (step === 0) {
+    prevBtn.setAttribute('disabled', 'disabled')
+  } else {
+    prevBtn.removeAttribute('disabled')
+  }
+
+  if (step === 2) {
+    nextBtn.innerHTML = '送出申請'
+  } else {
+    nextBtn.innerHTML = '下一步'
+  }
+}
+
+cardList.addEventListener('click', handleCardClicked)
+btnControl.addEventListener('click', handleBtnControlClicked)
